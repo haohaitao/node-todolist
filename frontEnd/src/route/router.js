63b24cf38f0bf0bf9256a5c66501b2845c2814d1
haohaitao <596336501@qq.com>
2021-07-27 17:57:03 +0800

    feat:使用hooks重构list文件

--- frontEnd/src/route/router.js
+++ frontEnd/src/route/router.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import {HashRouter, Route, Switch} from 'react-router-dom';
-import Home from '../page/home';
-import List from '../page/list';
-
+/*
+ * @Description  :
+ * @Author       : pacino
+ * @Date         : 2021-07-23 17:12:46
+ * @LastEditTime : 2021-07-27 17:56:24
+ * @LastEditors  : pacino
+ */
+import React from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Home from "../page/home";
+import List from "../page/list";
 
 // 配置路由
 const BasicRoute = () => (
-    <HashRouter>
-        <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/list" component={List}/>
-        </Switch>
-    </HashRouter>
+  <HashRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/list" component={List} />
+    </Switch>
+  </HashRouter>
 );
-
 
 export default BasicRoute;
--- frontEnd/src/utils/request.js
+++ frontEnd/src/utils/request.js
@@ -1,35 +1,53 @@
-import axios from 'axios'
-import { message } from 'antd'
+/*
+ * @Description  :
+ * @Author       : pacino
+ * @Date         : 2021-07-27 09:37:38
+ * @LastEditTime : 2021-07-27 17:55:37
+ * @LastEditors  : pacino
+ */
+import axios from "axios";
+import { Modal } from "antd";
 // 根据环境不同引入不同api地址
 // create an axios instance
 const service = axios.create({
   // baseURL: '', // url = base api url + request url
   // withCredentials: true // send cookies when cross-domain requests
-})
+});
 
 // request拦截器 request interceptor
 service.interceptors.request.use(
-  config => {
-    // 不传递默认开启loading
-    if (!config.hideloading) {
-      // loading
-    }
-    return config
+  (config) => {
+    return config;
   },
-  error => {
+  (error) => {
     // do something with request error
-    return Promise.reject(error)
+    console.log("---err0r1111----", error);
+    Modal.info({
+      title: "提示",
+      content: (
+        <div>
+          <p>系统出现异常，请稍后重试</p>
+        </div>
+      ),
+      onOk() {
+        return Promise.reject(error);
+      },
+    });
   }
-)
+);
 // respone拦截器
 service.interceptors.response.use(
-  response => {
-    return Promise.resolve(response)
+  (response) => {
+    return Promise.resolve(response);
   },
-  error => {
-    message.error('系统出现异常，请稍后重试')
-    return Promise.reject(error)
+  (error) => {
+    console.log("---err0r2222----", error);
+    Modal.error({
+      title: "提示",
+      content: "系统出现异常，请稍后重试",
+    });
+    return Promise.reject(error);
   }
-)
+);
 
-export default service
+export default service;
