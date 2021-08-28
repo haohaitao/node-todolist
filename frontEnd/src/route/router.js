571903d18749bd8ecac2469d621ee475349789e9
haohaitao <596336501@qq.com>
2021-08-29 00:14:43 +0800

    update

--- frontEnd/src/route/router.js
+++ frontEnd/src/route/router.js
@@ -1,23 +1,18 @@
-/*
- * @Description  :
- * @Author       : pacino
- * @Date         : 2021-07-23 17:12:46
- * @LastEditTime : 2021-07-27 17:56:24
- * @LastEditors  : pacino
- */
-import React from "react";
-import { HashRouter, Route, Switch } from "react-router-dom";
-import Home from "../page/home";
-import List from "../page/list";
+import React from 'react';
+import {HashRouter, Route, Switch} from 'react-router-dom';
+import Home from '../page/home';
+import List from '../page/list';
+
 
 // 配置路由
 const BasicRoute = () => (
-  <HashRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/list" component={List} />
-    </Switch>
-  </HashRouter>
+    <HashRouter>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/list" component={List}/>
+        </Switch>
+    </HashRouter>
 );
+
 
 export default BasicRoute;
--- frontEnd/src/setupProxy.js
+++ frontEnd/src/setupProxy.js
@@ -1,22 +1,12 @@
-/*
- * @Description  : 
- * @Author       : pacino
- * @Date         : 2021-07-27 09:37:38
- * @LastEditTime : 2021-07-27 11:04:41
- * @LastEditors  : pacino
- */
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: "https://nodejs.haoht123.com",
-      changeOrigin: true,
-      secure: false,
-      pathRewrite: {
-        "^/api": "/",
-      },
-    })
-  );
+  app.use('/api', createProxyMiddleware({
+    target: 'https://www.xxx.com',
+    changeOrigin: true,
+    secure: false,
+    pathRewrite: {
+      "^/api": "/"
+    },
+  }));
 };
